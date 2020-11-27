7bd8609547964eb5b7b4a392c0511f8e824ae3ad
Marta Casal Tàpia <>
2020-11-27 12:37:56 +0100

    deleting all coments on fase 4

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,6 +6,7 @@ function crearRocket(code, totalThruster, thrusters) {
     arrRockets.push(rocketCreat);
     console.log(arrRockets);
 }
+// mostrar informació del rocket i botons d'accelerar i frenar
 function showInfo(index) {
     var printRocket = document.getElementById('printRocket');
     printRocket.innerHTML = "\n        <div class=\"col-6 container mt-5\">\n            <p class=\"text-center pt-4 font-weight-bold text-lg\">Rocket " + (index + 1) + "</p>\n            <p class=\"text-center text-sm\">Code: " + arrRockets[index].code + "</p>\n            <p class=\"text-center text-sm\">Total thrusters: " + arrRockets[index].totalThruster + "</p>\n            <p class=\"text-center text-sm\">Max power: " + arrRockets[index].thrusters + "</p>\n            <p class=\"text-center text-sm\">Current Speed: <span id=\"currentSpeed" + (index + 1) + "\">" + arrRockets[index].startCounting() + "</span></p>\n            <p class=\"text-center text-sm\">Current Power: <span id=\"currentPower" + (index + 1) + "\">" + arrRockets[index].currentPower() + "</span></p>\n            <div class=\"text-center\">\n                <button type=\"button\" id=\"speedUpRocket" + (index + 1) + "\" class=\"btn btn-outline-light btn-sm mx-2 speed-up\">Speed Up</button>\n                <button type=\"button\" id=\"slowDownRocket" + (index + 1) + "\" class=\"btn btn-outline-light btn-sm mx-2 slow-down\">Slow Down</button>\n            </div>\n        </div>";
@@ -23,7 +24,7 @@ function showInfo(index) {
         rocketCurrentPower.innerText = "" + arrRockets[index].currentPower();
     };
 }
-// hi ha problemitas al show el num de rockets
+// informació de tots els rockets
 function showAll() {
     for (var i = 0; i < arrRockets.length; i++) {
         var printAllRockets = document.getElementById('printAllRockets');
@@ -32,46 +33,3 @@ function showAll() {
         printAllRockets.innerHTML += "\n        <div class=\"col-6 container mt-5\">\n            <p class=\"text-center pt-4 font-weight-bold text-lg\" id=\"rocketNum" + (i + 1) + "\">Rocket " + (i + 1) + "</p>\n            <p class=\"text-center text-sm\" id=\"rocketCode" + (i + 1) + "\">Code: " + arrRockets[i].code + "</p>\n            <p class=\"text-center text-sm\" id=\"rocketThrusters" + (i + 1) + "\">Total thrusters: " + arrRockets[i].totalThruster + "</p>\n            <p class=\"text-center text-sm\" id=\"rocketPower" + (i + 1) + "\">Max power: " + arrRockets[i].thrusters + "</p>\n        </div>";
     }
 }
-// mostrar informació de rocket
-//const infoRocket1 = document.getElementById('infoRocket1') as HTMLElement;
-//const infoRocket2 = document.getElementById('infoRocket2') as HTMLElement;
-/* for (let i = 0; i < arrRockets.length; i++) {
-    const infoRockets = document.getElementById('infoRockets') as HTMLElement;
-    // es crea un div per a cada rocket
-    //infoRockets.classList.remove("d-none");
-    infoRockets.innerHTML += `<div id="rocket${i + 1}"></div>`;
-
-    //es printa dins del div de cada rocket una platilla on es volcarà tota la informació
-    const rocketTemplate = document.getElementById(`rocket${i + 1}`) as HTMLElement;
-    rocketTemplate.innerHTML = `
-    <div class="col-6 container mt-5">
-        <p class="text-center pt-4 font-weight-bold text-lg" id="rocketNum${i + 1}">Rocket ${i + 1}</p>
-        <p class="text-center text-sm" id="rocketCode${i + 1}">Code: ${arrRockets[i].code}</p>
-        <p class="text-center text-sm" id="rocketThrusters${i + 1}">Total thrusters: ${arrRockets[i].totalThruster}</p>
-        <p class="text-center text-sm" id="rocketPower${i + 1}">Max power: ${arrRockets[i].thrusters}</p>
-        <p class="text-center text-sm">Current Speed: <span id="currentSpeed${i + 1}">${arrRockets[i].startCounting()}</span></p>
-        <p class="text-center text-sm">Current Power: <span id="currentPower${i + 1}">${arrRockets[i].currentPower()}</span></p>
-        <div class="text-center">
-            <button type="button" id="speedUpRocket${i + 1}" class="btn btn-outline-light btn-sm mx-2 speed-up">Speed Up</button>
-            <button type="button" id="slowDownRocket${i + 1}" class="btn btn-outline-light btn-sm mx-2 slow-down">Slow Down</button>
-        </div>
-    </div>`;
-} */
-// addEventListeners no funcionen: Property 'addEventListener' does not exist on type 'HTMLCollectionOf<Element>'.
-/* for (let i = 0; i < arrRockets.length; i++) {
-    const btnSpeedUp: any = document.getElementsByClassName('speed-up') as HTMLCollectionOf<HTMLButtonElement>;
-    const btnSlowDown: any = document.getElementsByClassName('slow-down') as HTMLCollectionOf<HTMLButtonElement>;
-
-    const rocketCurrentSpeed = document.getElementById(`currentSpeed${i + 1}`) as HTMLElement;
-    const rocketCurrentPower = document.getElementById(`currentPower${i + 1}`) as HTMLElement;
-
-    btnSpeedUp[i].onclick = function () {
-        rocketCurrentSpeed.innerText = `${arrRockets[i].speedUp()}`;
-        rocketCurrentPower.innerText = `${arrRockets[i].currentPower()}`;
-    }
-
-    btnSlowDown[i].onclick = function () {
-        rocketCurrentSpeed.innerText = `${arrRockets[i].slowDown()}`;
-        rocketCurrentPower.innerText = `${arrRockets[i].currentPower()}`;
-    }
-} */ 
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -7,6 +7,7 @@ function crearRocket(code: string, totalThruster: number, thrusters: Array<numbe
     console.log(arrRockets);
 }
 
+// mostrar informació del rocket i botons d'accelerar i frenar
 function showInfo(index: number) {
     const printRocket = document.getElementById('printRocket') as HTMLElement;
     printRocket.innerHTML = `
@@ -41,7 +42,7 @@ function showInfo(index: number) {
     }
 }
 
-// hi ha problemitas al show el num de rockets
+// informació de tots els rockets
 function showAll() {
     for (let i = 0; i < arrRockets.length; i++) {
         const printAllRockets = document.getElementById('printAllRockets') as HTMLElement;
@@ -56,48 +57,3 @@ function showAll() {
         </div>`;
     }
 }
-// mostrar informació de rocket
-//const infoRocket1 = document.getElementById('infoRocket1') as HTMLElement;
-//const infoRocket2 = document.getElementById('infoRocket2') as HTMLElement;
-
-/* for (let i = 0; i < arrRockets.length; i++) {
-    const infoRockets = document.getElementById('infoRockets') as HTMLElement;
-    // es crea un div per a cada rocket
-    //infoRockets.classList.remove("d-none");
-    infoRockets.innerHTML += `<div id="rocket${i + 1}"></div>`;
-
-    //es printa dins del div de cada rocket una platilla on es volcarà tota la informació
-    const rocketTemplate = document.getElementById(`rocket${i + 1}`) as HTMLElement;
-    rocketTemplate.innerHTML = `
-    <div class="col-6 container mt-5">
-        <p class="text-center pt-4 font-weight-bold text-lg" id="rocketNum${i + 1}">Rocket ${i + 1}</p>
-        <p class="text-center text-sm" id="rocketCode${i + 1}">Code: ${arrRockets[i].code}</p>
-        <p class="text-center text-sm" id="rocketThrusters${i + 1}">Total thrusters: ${arrRockets[i].totalThruster}</p>
-        <p class="text-center text-sm" id="rocketPower${i + 1}">Max power: ${arrRockets[i].thrusters}</p>
-        <p class="text-center text-sm">Current Speed: <span id="currentSpeed${i + 1}">${arrRockets[i].startCounting()}</span></p>
-        <p class="text-center text-sm">Current Power: <span id="currentPower${i + 1}">${arrRockets[i].currentPower()}</span></p>
-        <div class="text-center">
-            <button type="button" id="speedUpRocket${i + 1}" class="btn btn-outline-light btn-sm mx-2 speed-up">Speed Up</button>
-            <button type="button" id="slowDownRocket${i + 1}" class="btn btn-outline-light btn-sm mx-2 slow-down">Slow Down</button>
-        </div>
-    </div>`;
-} */
-
-// addEventListeners no funcionen: Property 'addEventListener' does not exist on type 'HTMLCollectionOf<Element>'.
-/* for (let i = 0; i < arrRockets.length; i++) {
-    const btnSpeedUp: any = document.getElementsByClassName('speed-up') as HTMLCollectionOf<HTMLButtonElement>;
-    const btnSlowDown: any = document.getElementsByClassName('slow-down') as HTMLCollectionOf<HTMLButtonElement>;
-
-    const rocketCurrentSpeed = document.getElementById(`currentSpeed${i + 1}`) as HTMLElement;
-    const rocketCurrentPower = document.getElementById(`currentPower${i + 1}`) as HTMLElement;
-
-    btnSpeedUp[i].onclick = function () {
-        rocketCurrentSpeed.innerText = `${arrRockets[i].speedUp()}`;
-        rocketCurrentPower.innerText = `${arrRockets[i].currentPower()}`;
-    }
-
-    btnSlowDown[i].onclick = function () {
-        rocketCurrentSpeed.innerText = `${arrRockets[i].slowDown()}`;
-        rocketCurrentPower.innerText = `${arrRockets[i].currentPower()}`;
-    }
-} */
